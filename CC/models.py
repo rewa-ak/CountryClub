148a0928f49121e0f824cb5c3269619c6db49a74
Rewa Abou Karroum <rewaak@gmail.com>
2022-04-14 10:32:42 +0300

    forms

--- CC/models.py
+++ CC/models.py
@@ -1,30 +1,33 @@
 from django.db import models
+from django.contrib import admin
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
+
+class Facilities(models.Model):
+    facility=models.CharField(max_length=20)
+
+class Membership_type(models.Model):
+    Type=models.CharField(max_length=30)
+    Facilities=models.ForeignKey(Facilities,on_delete=models.SET_NULL)
+    Duration=models.DurationField()
 
 class Member(models.Model):
     First_name= models.CharField(max_length=15)
     Last_name= models.CharField(max_length=15)
     Date_of_birth= models.DateField()
-    Email=models.EmailField()
-    Membership_type= models.OneToOneField(Membership_type)
+    Email=models.EmailField(unique=True)
+    Membership_type= models.OneToOneField(Membership_type,on_delete=models.CASCADE)
     Membership_deadline= models.DateField()
-    Reservation= models.ManyToManyField(Reservation)
+    
 
-class Membership_type:
-    Type=models.CharField(Max_length=30)
-    Facilities=models.ManyToManyField(Facilities)
-    Duration=models.DurationField()
-
-class Facilities:
-    facility=models.CharField(max_length=20)
-
-
-class Reservation:
+class Reservation(models.Model):
     Member=models.ManyToManyField(Member)
     Reserved_date=models.DateField()
     Reserved_time_start= models.TimeField()
     Reserved_time_end= models.TimeField()
-    Facility=models.OneToOneField(Facilities)
+    Facility=models.OneToOneField(Facilities,on_delete=models.CASCADE)
+    Court_number= models.IntegerField()
 
 
